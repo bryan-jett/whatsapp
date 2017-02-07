46dbf16c6ccf24bf8dd75e3b29d5ece9820f9d81
Bryan Jett <bryan.jett@experian.com>
2017-02-06 17:39:22 -0800

    Modified boilerplate code for whatsapp tutorial

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import template from "./app.component.html";
 import style from "./app.component.scss";
+import {StatusBar} from "ionic-native";
+import {Platform} from "ionic-angular";
 
 @Component({
   selector: "app",
@@ -8,6 +10,11 @@ import style from "./app.component.scss";
   styles: [ style ]
 })
 export class AppComponent {
-  constructor() {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
   }
 }
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
-import { DemoComponent } from "./demo/demo.component";
-import { DemoDataService } from "./demo/demo-data.service";
+import {IonicApp, IonicModule} from "ionic-angular";
 
 @NgModule({
   // Components, Pipes, Directive
   declarations: [
-    AppComponent,
-    DemoComponent
+    AppComponent
   ],
   // Entry Components
   entryComponents: [
@@ -16,14 +13,13 @@ import { DemoDataService } from "./demo/demo-data.service";
   ],
   // Providers
   providers: [
-    DemoDataService
   ],
   // Modules
   imports: [
-    BrowserModule
+    IonicModule.forRoot(AppComponent)
   ],
   // Main Component
-  bootstrap: [ AppComponent ]
+  bootstrap: [ IonicApp ]
 })
 export class AppModule {
   constructor() {
